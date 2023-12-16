c8b6a78a0900dd31267164d6c4b8264cc2f7c581
MOHAMMED MURSHID ALAM <83682620+murshidalam7474@users.noreply.github.com>
2023-12-16 22:29:05 +0530

    Add files via upload

---
+++ client.py
@@ -0,0 +1,26 @@
+# import socket
+# HOST ='192.168.29.176'
+# PORT=9090
+# socket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+# socket.connect((HOST,PORT))
+# socket.send("hello world".encode('utf-8'))
+# print(socket.recv(1024).decode('utf-8'))
+import socket
+
+HOST = '192.168.29.176'
+PORT = 9090
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((HOST, PORT))
+
+while True:
+    message = input("Enter your message (type 'quit' to exit): ")
+    client_socket.send(message.encode('utf-8'))
+
+    if message.lower() == 'quit':
+        break
+
+    response = client_socket.recv(1024).decode('utf-8')
+    print(f"Server's reply: {response}")
+
+client_socket.close()
---
+++ server.py
@@ -0,0 +1,41 @@
+# import socket
+# #bind
+# HOST = '192.168.29.176'
+# PORT = 9090
+# server=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+# server.bind((HOST,PORT))
+# server.listen(5)
+# while True:
+#     comm_sock, address=server.accept()
+#     print(f"connected to {address}")
+#     message=comm_sock.recv(1024).decode('utf-8')
+#     print(f"message from client is ->  {message}")
+#     comm_sock.send(f"hey got ur message! thank you".encode('utf-8'))
+#     comm_sock.close()
+#     print(f"connection with {address} closeddd!")
+import socket
+
+# bind
+HOST = '192.168.29.176'
+PORT = 9090
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((HOST, PORT))
+server.listen(5)
+
+while True:
+    comm_sock, address = server.accept()
+    print(f"Connected to {address}")
+
+    while True:
+        message = comm_sock.recv(1024).decode('utf-8')
+        print(f"Message from client is -> {message}")
+
+        if message.lower() == 'quit':
+            break
+
+        reply = input("Enter your reply: ")
+        comm_sock.send(reply.encode('utf-8'))
+
+    comm_sock.close()
+    print(f"Connection with {address} closed!")
